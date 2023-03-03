a0d1cc358cf14ad1b60eeadb82a24b102809c9d6
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2023-03-03 18:57:33 +0400

    Basic Ws server

---
+++ core.js
@@ -0,0 +1,70 @@
+//Initially it will work with IP whitelisting
+import WebSocket, { WebSocketServer } from 'ws';
+
+
+const wss = new WebSocketServer({ port: 8080 });
+const keepAlive = 10000;
+const keepAliveMsg =  JSON.stringify({"MESSAGE_TYPE":'KeepAlive',"INTERVAL":'10'});
+const EXECUTIONREPORT =  JSON.stringify({"MESSAGE_TYPE":'EXECUTIONREPORT',"INF":{}});
+const ORDER =  JSON.stringify({"MESSAGE_TYPE":'ORDER',"INF":{}});
+
+
+
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+    broadcastKeepalive(ws);
+    ws.on('message', function message(data) {
+        let msg = JSON.parse(data);
+        msgHandler(msg, ws);
+    });
+    ws.on('spawn',()=>{
+        console.log('New User S');
+    });
+});
+
+
+function msgHandler(msg, ws) {
+    switch (msg.MessageType) {
+        case 'ExecutionReport': {
+            console.log('I got the report');
+            break;
+        }
+        case 'TokenInf': {
+            console.log('Needs to send token Information');
+            break;
+        }
+        case 'Order': {
+            console.log('I got the order');
+            break;
+        }
+    }
+}
+
+
+
+async function broadcastKeepalive(ws) {
+    setTimeout(() => {broadcastKeepalive(ws);}, keepAlive);
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            ws.send(keepAliveMsg);
+        }
+    });
+}
+
+
+
+
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    console.log("...");
+  });
+
+  process.on('TypeError', function (err) {
+    console.error(err);
+    console.log("...");
+  });
+
+
+
+
