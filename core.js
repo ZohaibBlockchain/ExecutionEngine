9ea4539b1cbeda157e945c1bf3a2c675b02d0161
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2023-03-06 12:59:15 +0400

    Port update

--- core.js
+++ core.js
@@ -2,7 +2,7 @@
 import WebSocket, { WebSocketServer } from 'ws';
 
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 7100 });
 const keepAlive = 500;
 const keepAliveMsg =  JSON.stringify({"MESSAGE_TYPE":'KeepAlive',"INTERVAL":'10'});
 const EXECUTIONREPORT =  JSON.stringify({"MESSAGE_TYPE":'EXECUTIONREPORT',"INF":{}});
